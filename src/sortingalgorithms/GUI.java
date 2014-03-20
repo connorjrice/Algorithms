238c99aad6d564a1f23204c259a4852f1738a686
clarence <clarence@cr>
2014-03-20 11:45:29 -0600

    GUI

--- src/sortingalgorithms/GUI.java
+++ src/sortingalgorithms/GUI.java
@@ -1,9 +1,79 @@
 package sortingalgorithms;
 
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.asset.AssetManager;
+import com.jme3.audio.AudioRenderer;
+import com.jme3.input.InputManager;
+import com.jme3.renderer.ViewPort;
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
+
 /**
  *
- * @author clarence
+ * @author Connor Rice
  */
-public class GUI {
+public class GUI extends AbstractAppState implements ScreenController {
+    
+    private Nifty nifty;
+    private Screen screen;
+    private AssetManager assetManager;
+    private AudioRenderer audioRenderer;
+    private ViewPort guiViewPort;
+    private InputManager inputManager;
+    private SimpleApplication app;
+    
+    public GUI() {
+        
+    }
+    
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        super.initialize(stateManager, app);
+        this.app = (SimpleApplication) app;
+        
+    }
+    
+    public void quickSort() {
+        
+    }
+    
+    public void mergeSort() {
+        
+    }
+    
+    public void exchangeSort() {
+        
+    }
+    
+    @Override
+    public void update(float tpf) {
+        //TODO: implement behavior during runtime
+    }
+
+    @Override
+    public void bind(Nifty nifty, Screen screen) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void onStartScreen() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void onEndScreen() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public void cleanup() {
+        super.cleanup();
+        //TODO: clean up what you initialized in the initialize method,
+        //e.g. remove all spatials from rootNode
+        //this is called on the OpenGL thread after the AppState has been detached
+    } 
     
 }
--- src/sortingalgorithms/SortingAlgorithms.java
+++ src/sortingalgorithms/SortingAlgorithms.java
@@ -1,15 +1,63 @@
 package sortingalgorithms;
 
+import com.jme3.app.SimpleApplication;
+import com.jme3.niftygui.NiftyJmeDisplay;
+import com.jme3.system.AppSettings;
+import com.jme3.system.NanoTimer;
+import de.lessvoid.nifty.Nifty;
+import java.awt.DisplayMode;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Connor Rice
  */
-public class SortingAlgorithms {
+public class SortingAlgorithms extends SimpleApplication {
+    
+    private GUI GUIState;
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        
+        AppSettings sets = new AppSettings(false);
+        SortingAlgorithms app = new SortingAlgorithms();
+        sets.setFrameRate(60);
+        sets.setTitle("Sorting Algorithms");
+ 
+        app.start();
+        // Don't bother me logging
+        Logger.getLogger("").setLevel(Level.SEVERE);
     }
+
+    @Override
+    public void simpleInitApp() {
+        setTimer(new NanoTimer());
+        flyCam.setDragToRotate(true);
+        inputManager.setCursorVisible(true);
+        flyCam.setRotationSpeed(0.0f);
+
+        setDisplayFps(false);
+        setDisplayStatView(false);
+        startGUI();
+    }
+    @Override
+    public void simpleUpdate(float tpf) {
+
+    }
+    
+    public void startGUI() {
+        NiftyJmeDisplay niftyDisplay = new NiftyJmeDisplay(assetManager,
+                                                          inputManager,
+                                                          audioRenderer,
+                                                          guiViewPort);
+        Nifty nifty = niftyDisplay.getNifty();
+ 
+        guiViewPort.addProcessor(niftyDisplay);
+        GUIState = new GUI();
+        stateManager.attach(GUIState);
+        nifty.registerScreenController(GUIState);
+        nifty.fromXml("assets/GUI.xml", "start");
+    }
+    
 }
