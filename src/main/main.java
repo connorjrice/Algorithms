cc96710f0ef59b1dc7654893bf37bb48935fb77a
Connor Rice <connor.rice@coloradocollege.edu>
2016-03-21 20:03:14 -0600

    Experimenting with selector console, binary search.

---
+++ src/main/main.java
@@ -0,0 +1,22 @@
+package main;
+
+import searchalgorithms.BinarySearch;
+
+/**
+ *
+ * @author Connor
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        binarySearch();
+    }
+    
+    public static void binarySearch() {
+        BinarySearch bs = new BinarySearch();
+        System.out.println(bs.find());
+    }
+}
--- src/searchalgorithms/BinarySearch.java
+++ src/searchalgorithms/BinarySearch.java
@@ -4,20 +4,52 @@ package searchalgorithms;
  *
  * @author Connor Rice
  */
+
 public class BinarySearch {
-    private String[] searchList;
+
+    protected int low, mid, high;
+    protected int location;
+    protected int[] s;
+    protected int x;
+
     
-    public BinarySearch(String[] _list) {
-        searchList = _list;
-    }
- 
-    private int binarySearch(String query) {
-        
-        return 0;
-    }
-    
-    public int getPos(String query) {
-        return binarySearch(query);
+    public BinarySearch() {
+        this.high = 5;
+        this.s = new int[]{0,1,2,3,4,5}; // I'm using 0 as a empty character
+        this.x = 3;                      // and it troubles me.
+        this.low = 1;
+        this.location = 0;
     }
     
+    /**
+     * n = index, 1 to n represents the list
+     * s = primitive integer array
+     * x = query
+     * l = location
+     **/
+    public BinarySearch(int n, int[] s, int x, int l) {
+	this.high = n;
+	this.s = s;
+	this.x = x;
+	this.low = 1;
+	this.location = 0;
+    }
+
+    public int find() {
+	while (low <= high && location == 0) {
+	    mid = low + high/2;
+	    if (x == s[mid]) {
+		location = mid;
+	    } else if (x < s[mid]) {
+		high = mid - 1;
+	    } else {
+		low = mid + 1;
+	    }
+
+        }
+        return location;
+    }
+    
+    
+
 }
