4c312ab8ff43f1f6f72da09c53119a09b304f4be
Connor Rice <connor.rice@coloradocollege.edu>
2016-03-22 20:00:47 -0600

    changed name

---
+++ src/datastructure/FastList.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructure;
+
+/**
+ * This data structure will be a dynamically sizeable singly linked list, to
+ * replace all instances of ArrayLists in the code.
+ * @author Connor
+ */
+public class FastList {
+    
+}
