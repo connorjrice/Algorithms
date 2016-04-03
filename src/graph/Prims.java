a47951ee003cf031e3d27658bcdaa6488c2785d1
Connor Rice <connor.rice@coloradocollege.edu>
2016-04-02 22:45:04 -0600

    prims seems to work.

--- src/graph/Prims.java
+++ src/graph/Prims.java
@@ -28,51 +28,41 @@ public class Prims {
         int numEdges = 0;
                 
         // Edge is startEdge, endEdge, weight (a[0], a[1], a[2])
-        //int[] e = new int[3];
         int vnear = 0; // index of nearest node
         double min = Double.POSITIVE_INFINITY; // value // thanks book for infinity
-        int[] nearest = new int[W[0].length-1]; // index
-        int[] distance = new int[W[0].length-1]; // value
+        int[] nearest = new int[W[0].length]; // index
+        int[] distance = new int[W[0].length]; // value
         
         // For all vertices, initalize v1 to be the nearest vertex in Y and init
         // ialize the distance from Y to be the weight on the edge to v1.
-        for (int i = 2; i <= n; i++) {
+        for (int i = 1; i < n; i++) {
             nearest[i] = 1;
             distance[i] = W[0][i];
         }
         for (int i = 0; i < n; i++) { // repeat n-1 times
             min = Double.POSITIVE_INFINITY; // value // thanks book for infinity
             
-            for (int j = 2; j <= n; j++) { // Check each vertex for being nearest
+            for (int j = 1; j < n; j++) { // Check each vertex for being nearest
                                            // to Y (current vertex)
-                if (0 <= distance[i] && distance[i] < min) {
-                    min = distance[i];
-                    vnear = i;
+                if (0 <= distance[j] && distance[j] < min) {
+                    min = distance[j];
+                    vnear = j;
                 }                
             }
             // Add this edge to F
             F[numEdges] = new int[]{vnear,nearest[vnear],W[vnear][nearest[vnear]]}; 
+            numEdges++;
             distance[vnear] = -1;
             
             // Add vertex indexed by vnear to Y.
             // For each vertex not in Y, update it's distance from Y.
-            for (int j = 2; j <= n; i++) {
+            for (int j = 1; j < n; j++) {
                 if (W[j][vnear] < distance[j]) {
-                    distance[i] = W[j][vnear];
-                    nearest[i] = vnear;
+                    distance[j] = W[j][vnear];
+                    nearest[j] = vnear;
                 }
             }            
         }
-        
-
-        
-        //e = new int[]{vnear,nearest[vnear],W[vnear][nearest[vnear]]};
-        
-        
-        
-        
-        
-        
         return F;
     }
     
